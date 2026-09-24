438578732fe99988af6f86e622194e024ba4de95
23010077-rgb <23010077@st.phenikaa-uni.edu.vn>
2026-09-24 07:30:51 +0700

    Tạo Entity

---
+++ src/student/entities/student.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Student {
+  @PrimaryColumn({ length: 10 })
+  SID: string;
+
+  @Column({ length: 30 })
+  SNAME: string;
+
+  @Column({ length: 30 })
+  EMAIL: string;
+
+  @Column({ length: 10, nullable: true })
+  Tutor_id: string;
+}
